9831c1ca393ab8b47f0233a105e84192675c784c
tpol1473 <tpol1473@gmail.com>
2021-11-24 16:55:02 +0200

    last commit

--- js/lecture_1/script.js
+++ js/lecture_1/script.js
@@ -1,36 +1,11 @@
-// function changeColor(){
-//     const testParagraf = document.getElementById("test-id");
-//     testParagraf.style.color = "red"; 
-// }
-
-// const myButton = document.getElementById("button");
-// myButton.addEventListener("click", changeColor);
-
-// function changeColor(){
-//         const testParagraf = document.getElementById("test-id");
-//         testParagraf.style.color = color; 
-//     }
-    
-//     const myButton = document.getElementById("button");
-//     myButton.addEventListener("click", () => changeColor ("red"));
-//     myButton.addEventListener("mouselive", () => changeColor ("blue"));
-
-function displaynone(){
-    const testParagraf = document.getElementById("test-id");
-    testParagraf.style.display = "none"; 
-}
-
-const myBtn = document.getElementById("btn");
-myBtn.addEventListener("click", displaynone);
-
-
-const testParagraf = document.getElementById("test-id")
-
 function hideElement(){
+    const testParagraf = document.getElementById("logo");
     testParagraf.style.display = "none"; 
 }
+
 
 function showElement(){
+    const testParagraf = document.getElementById("logo");
     testParagraf.style.display = "block"; 
 }
 
@@ -39,10 +14,3 @@ const btnShow = document.getElementById("btn-show");
 
 btnHide.addEventListener("click", hideElement);
 btnShow.addEventListener("click", showElement);
-
-
-for (let i = 0; i == 10000; i++) {
-    console.log("test");
-}
-
-if
