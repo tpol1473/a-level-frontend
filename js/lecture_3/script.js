a1deb2112729fda8210a12018c67789256408ffd
tpol1473 <tpol1473@gmail.com>
2021-12-06 18:33:50 +0200

    commit 0612

--- js/lecture_3/script.js
+++ js/lecture_3/script.js
@@ -71,21 +71,53 @@
 
 // console.log(`Cat name is ${cat.say()}`)
 
-const user = {
-    name: 'Tatiana',
-    age: 18,
-    isMale: false,
-    say: function(){
-        alert('Hallo!');
-    }
-};
+// const user = {
+//     name: 'Tatiana',
+//     age: 18,
+//     isMale: false,
+//     say: function(){
+//         alert('Hallo!');
+//     }
+// };
 // typeof cat;
 // typeof cat.age;
 
 
-console.log(`My name is ${user.say()}`)
+// console.log(`My name is ${user.say()}`)
 
-const Sym1 = Symbol("Sym")
-const Sym2 = Symbol("Sym")
+// const Sym1 = Symbol("Sym")
+// const Sym2 = Symbol("Sym")
 
 //console.log(return Sym1 == Sym2); // false
+
+// let a = 1;
+// let b = 2;
+// const c = 8;
+
+// function makeSomeCoolStaff() { 
+//     var c = 5;
+
+//     console.log(a); // 1. a = 7
+//     console.log(b); // 2. b = undefined
+//     console.log(c); // 3. c = 5
+//     if (true) {
+//         a = 3;
+//         var b = 4;
+        
+//         console.log(a); // 4. a = 3
+//         console.log(b); // 5. b = 4
+//         console.log(c); // 6. c = 5
+//     }
+    
+//     console.log(a); // 7. a = 3
+//     console.log(b); // 8. b = 4
+//     console.log(c); // 9. c = 5
+// }
+
+// a = 7;
+// makeSomeCoolStaff();
+
+// console.log(a); // 10. a = 3
+// console.log(b); // 11. b = 2
+// console.log(c); // 12. c = 8
+
---
+++ js/lecture_3/script_.js
@@ -0,0 +1,15 @@
+function greeting(name) {
+    let name = prompt('What is your name?', );
+
+    alert(`Hello, ${name} How are you?`); // 'Hello, Valera! How are you?'
+
+}
+
+// const myButton = document.getElementById('Click');
+// myButton.addEventListener('click', alert)
+
+const lecture = {
+
+    
+}
+
