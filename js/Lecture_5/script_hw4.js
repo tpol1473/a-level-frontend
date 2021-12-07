79cd9ee4fe909286e4f6e530dea65b9c67f1f770
tpol1473 <94137802+tpol1473@users.noreply.github.com>
2021-12-07 22:43:49 +0200

    Task 3

---
+++ js/Lecture_5/script_hw4.js
@@ -0,0 +1,16 @@
+/** 
+* Task 3
+*/
+
+function searchSubstringAmount(initialString, searchingSubstring) {
+
+}
+
+/** 
+* Example
+* const initialString = 'Hello world';
+* searchSubstringAmount (initialString, 'world'); // 1
+* searchSubstringAmount (initialString, 'o'); // 2
+* searchSubstringAmount (initialString, 'l'); // 3
+* searchSubstringAmount (initialString, 'cat'); // There is not 'cat' in the initial string.;
+*/
