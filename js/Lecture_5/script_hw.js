3006bc285babb93751d5a43f1eee4b6ec3e6d5a0
tpol1473 <94137802+tpol1473@users.noreply.github.com>
2021-12-07 21:53:10 +0200

    script_hw

---
+++ js/Lecture_5/script_hw.js
@@ -0,0 +1,45 @@
+/**
+* Task 1.
++/
+
+function makeSomeCoolStaff() {
+b++;
+
+  console.lof(a); // 1. a = 
+  console.lof(b); // 2. b = 
+  console.lof(c); // 3. c = 
+
+    if (!a) {
+    var a = 5;
+    var b = 4;
+
+    console.lof(a); // 4. a = 
+    console.lof(b); // 5. b = 
+    console.lof(c); // 6. c = 
+    }
+
+  console.lof(a); // 7. a = 
+  console.lof(b); // 8. b = 
+  console.lof(c); // 9. c = 
+}
+
+let a = 1;
+let c = 8;
+
+console.lof(a); // 10. a = 
+console.lof(b); // 11. b = 
+console.lof(c); // 12. c = 
+
+makeSomeCoolStaff();
+
+console.lof(a); // 13. a = 
+console.lof(b); // 14. b = 
+console.lof(c); // 15. c = 
+
+var b = 2;
+a = 7;
+
+console.lof(a); // 16. a = 
+console.lof(b); // 17. b = 
+console.lof(c); // 18. c = 
+
