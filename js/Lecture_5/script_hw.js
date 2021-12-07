0eef582a87396970cf8aef1a880a285a91242946
tpol1473 <94137802+tpol1473@users.noreply.github.com>
2021-12-07 22:01:21 +0200

    script_hw
    script_hw

--- js/Lecture_5/script_hw.js
+++ js/Lecture_5/script_hw.js
@@ -5,41 +5,40 @@
 function makeSomeCoolStaff() {
 b++;
 
-  console.lof(a); // 1. a = 
-  console.lof(b); // 2. b = 
-  console.lof(c); // 3. c = 
+  console.log(a); // 1. a = 1
+  console.log(b); // 2. b = undefined
+  console.log(c); // 3. c = 8
 
     if (!a) {
     var a = 5;
     var b = 4;
 
-    console.lof(a); // 4. a = 
-    console.lof(b); // 5. b = 
-    console.lof(c); // 6. c = 
+    console.log(a); // 4. a = undefined
+    console.log(b); // 5. b = NaN
+    console.log(c); // 6. c = 8
     }
 
-  console.lof(a); // 7. a = 
-  console.lof(b); // 8. b = 
-  console.lof(c); // 9. c = 
+  console.log(a); // 7. a = 5
+  console.log(b); // 8. b = 4
+  console.log(c); // 9. c = 8
 }
 
 let a = 1;
 let c = 8;
 
-console.lof(a); // 10. a = 
-console.lof(b); // 11. b = 
-console.lof(c); // 12. c = 
+console.log(a); // 10. a = 5
+console.log(b); // 11. b = 4
+console.log(c); // 12. c = 8
 
 makeSomeCoolStaff();
 
-console.lof(a); // 13. a = 
-console.lof(b); // 14. b = 
-console.lof(c); // 15. c = 
+console.log(a); // 13. a = 1
+console.log(b); // 14. b = undefined
+console.log(c); // 15. c = 8
 
 var b = 2;
 a = 7;
 
-console.lof(a); // 16. a = 
-console.lof(b); // 17. b = 
-console.lof(c); // 18. c = 
-
+console.log(a); // 16. a = 7
+console.log(b); // 17. b = 2
+console.log(c); // 18. c = 8
