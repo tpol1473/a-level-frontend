bc86cbfaeed8bf11d98539373388e8f89b3f2bf2
tpol1473 <94137802+tpol1473@users.noreply.github.com>
2021-12-07 22:39:50 +0200

    task 2

---
+++ js/Lecture_5/script_hw3.js
@@ -0,0 +1,14 @@
+/** 
+* Task 2
+*/
+
+function searchSubstringIndex(initialString, searchingSubstring) {
+
+}
+
+/** 
+* Example
+* const initialString = 'Hello world';
+* searchSubstringIndex (initialString, 'world'); // 6
+* searchSubstringIndex (initialString, 'cat'); // There is not 'cat' in the initial string.;
+*/
