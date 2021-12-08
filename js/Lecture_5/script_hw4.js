b9bd664eb2b6b5ea54ea0884e0e51edf6d19095f
tpol1473 <94137802+tpol1473@users.noreply.github.com>
2021-12-08 22:07:13 +0200

    commit

--- js/Lecture_5/script_hw4.js
+++ js/Lecture_5/script_hw4.js
@@ -2,9 +2,30 @@
 * Task 3
 */
 
-function searchSubstringAmount(initialString, searchingSubstring) {
+function searchSubstringAmount (initialString, searchingSubstring) {
+    let initialString = prompt('Enter your search data:', );
+    let searchingSubstring = prompt('Enter the desired value:', );
+    let value = 0;
+    let result;
+    let arr = initialString.split(' ');
+  
+    //value = (initialString.split(${searchingSubstring}).length - 1);
+    //console.log(initialString.split(${searchingSubstring}).length - 1); //
+    //console.count(initialString.includes(${searchingSubstring}));
+  
+    arr.forEach(function(value) {
+      if (arr[value] = searchingSubstring) {
+        ++value;
+        result =  `'Count of the character: ' + value + '.'`;
+      else
+        result = 'Data not found.';
+      }
+    console.log(result);
+    return result;
+    }
 
-}
+
+searchSubstringAmount(initialString, searchingSubstring);  // or searchSubstringAmount(); ??????  
 
 /** 
 * Example
