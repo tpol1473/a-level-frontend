a8bdcaf11d592b8fc16f3d6c595cb5cf16bbe031
tpol1473 <94137802+tpol1473@users.noreply.github.com>
2021-12-27 18:52:45 +0200

    commit

---
+++ js/Lecture_7/script.js
@@ -0,0 +1,45 @@
+/*
+ Task 1
+ */
+ const findVowelsCount = (inputString) => {
+ 
+ if inputString.includes(a, e, i, o, u, y) i++;
+ returne i;
+ else
+ returne string = 'Data not found';
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ /*
+ Task 2
+ */
+ 
+ // Part 1
+  const createArraysWithObjects = (firstName, lastName) => ({firstName: firstName, lastName: lastName}));
+	
+	console.log(result);
+	
+ 
+ // Part 2
+	const createArraysWithObjects2 = (firstName, lastName, age) => ({firstName: firstName, lastName: lastName, age: age}));
+	
+	console.log(result);
+  
+  
+  
+  /*
+ Task 3
+ */
+ 
+  const filterArray = (arr, conditionNumber) => {
+	let result = arr.filter(element => element > conditionNumber);
+	console.log(result); //  
+  
+  
+ 
+ 
+ 
